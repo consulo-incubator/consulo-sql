4a5d2102636c3a9d804bc7edb843006bcc28fad2
VISTALL <vistall.valeriy@gmail.com>
2013-10-10 21:47:25 +0300

    removed not needed actions

--- src/com/dci/intellij/dbn/common/Icons.java
+++ src/com/dci/intellij/dbn/common/Icons.java
@@ -12,10 +12,6 @@ import com.intellij.ui.RowIcon;
 public class Icons {
     private static final Map<String, Icon> REGISTERED_ICONS = new THashMap<String, Icon>();
 
-    public static final Icon DBN_SPLASH = load("/img/DBN.png");
-    public static final Icon DONATE = load("/img/Donate.png");
-    public static final Icon DONATE_DISABLED = load("/img/DonateDisabled.png");
-
     public static final Icon COMMON_INFO = load("/img/v1/common/Info.png");
     public static final Icon COMMON_INFO_DISABLED = load("/img/v1/common/InfoDisabled.png");
     public static final Icon COMMON_WARNING = load("/img/v1/common/WarningTriangle.png");
