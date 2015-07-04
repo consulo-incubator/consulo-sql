0a2025818ac960ae72b534b4ab33e88c21e18a94
VISTALL <vistall.valeriy@gmail.com>
2015-07-04 13:15:35 +0300

    use light impl

--- src/com/dci/intellij/dbn/navigation/psi/DBObjectPsiDirectory.java
+++ src/com/dci/intellij/dbn/navigation/psi/DBObjectPsiDirectory.java
@@ -4,407 +4,213 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.swing.Icon;
-
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredReadAction;
 import com.dci.intellij.dbn.connection.GenericDatabaseElement;
-import com.dci.intellij.dbn.language.common.psi.EmptySearchScope;
 import com.dci.intellij.dbn.object.common.DBObject;
 import com.dci.intellij.dbn.object.common.list.DBObjectList;
 import com.dci.intellij.dbn.object.common.list.DBObjectListContainer;
-import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
-import com.intellij.lang.LanguageVersion;
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileSystemItem;
-import com.intellij.psi.PsiInvalidElementAccessException;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.ResolveState;
-import com.intellij.psi.scope.PsiScopeProcessor;
-import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.impl.light.LightElement;
 import com.intellij.psi.search.PsiElementProcessor;
-import com.intellij.psi.search.SearchScope;
 import com.intellij.util.IncorrectOperationException;
-import com.intellij.util.LanguageVersionUtil;
 
-public class DBObjectPsiDirectory extends UserDataHolderBase implements PsiDirectory, Disposable{
-    private DBObject object;
+public class DBObjectPsiDirectory extends LightElement implements PsiDirectory, Disposable
+{
+	private DBObject object;
 
-    public DBObjectPsiDirectory(DBObject object) {
-        this.object = object;
-    }
+	public DBObjectPsiDirectory(DBObject object)
+	{
+		super(PsiManager.getInstance(object.getProject()), Language.ANY);
+		this.object = object;
+	}
 
-    public DBObject getObject() {
-        return object;
-    }
+	public DBObject getObject()
+	{
+		return object;
+	}
 
-    @Override
-    public void dispose() {
-        object = null;
-    }
+	@Override
+	public void dispose()
+	{
+		object = null;
+	}
 
-    /*********************************************************
-     *                      PsiElement                       *
-     *********************************************************/
-    @NotNull
-    public String getName() {
-        return getObject().getName();
-    }
+	@Override
+	public String toString()
+	{
+		return object.toString();
+	}
 
-    public ItemPresentation getPresentation() {
-        return getObject();
-    }
+	@Override
+	@NotNull
+	public String getName()
+	{
+		return getObject().getName();
+	}
 
-    public FileStatus getFileStatus() {
-        return FileStatus.NOT_CHANGED;
-    }
+	@Override
+	public ItemPresentation getPresentation()
+	{
+		return getObject();
+	}
 
-    @NotNull
-    public Project getProject() throws PsiInvalidElementAccessException {
-        return getObject().getProject();
-    }
+	@Override
+	public PsiDirectory getParent()
+	{
+		GenericDatabaseElement parent = object.getTreeParent();
+		if(parent instanceof DBObjectList)
+		{
+			DBObjectList objectList = (DBObjectList) parent;
+			return NavigationPsiCache.getPsiDirectory(objectList);
+		}
 
-    @NotNull
-    public Language getLanguage() {
-        return Language.ANY;
-    }
+		return null;
+	}
+
+	@Override
+	public void navigate(boolean requestFocus)
+	{
+		getObject().navigate(requestFocus);
+	}
+
+	@Override
+	public boolean canNavigate()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canNavigateToSource()
+	{
+		return false;
+	}
+
+	@RequiredReadAction
+	@Override
+	@NotNull
+	public PsiElement[] getChildren()
+	{
+		List<PsiElement> children = new ArrayList<PsiElement>();
+		DBObjectListContainer childObjects = getObject().getChildObjects();
+		if(childObjects != null)
+		{
+			Collection<DBObjectList<DBObject>> objectLists = childObjects.getObjectLists();
+			if(objectLists != null)
+			{
+				for(DBObjectList objectList : objectLists)
+				{
+					children.add(NavigationPsiCache.getPsiDirectory(objectList));
+				}
+				return children.toArray(new PsiElement[children.size()]);
+			}
+		}
+
+		return PsiElement.EMPTY_ARRAY;
+	}
+
+	@Override
+	@NotNull
+	public VirtualFile getVirtualFile()
+	{
+		return getObject().getVirtualFile();
+	}
+
+	@Override
+	public boolean processChildren(PsiElementProcessor<PsiFileSystemItem> processor)
+	{
+		return false;
+	}
+
+	@Override
+	@NotNull
+	public PsiElement setName(@NotNull String name) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
+
+	@Override
+	public boolean isDirectory()
+	{
+		return true;
+	}
+
+	@Override
+	public void checkSetName(String name) throws IncorrectOperationException
+	{
+
+	}
+
+	@Override
+	public PsiDirectory getParentDirectory()
+	{
+		return getParent();
+	}
 
 	@NotNull
 	@Override
-	public LanguageVersion getLanguageVersion()
+	public PsiDirectory[] getSubdirectories()
 	{
-		return LanguageVersionUtil.findDefaultVersion(getLanguage());
+		return PsiDirectory.EMPTY_ARRAY;
 	}
 
-	public PsiDirectory getParent() {
-        GenericDatabaseElement parent = object.getTreeParent();
-        if (parent instanceof DBObjectList) {
-            DBObjectList objectList = (DBObjectList) parent;
-            return NavigationPsiCache.getPsiDirectory(objectList);
-        }
-
-        return null;
-    }
-
-    public ASTNode getNode() {
-        return null;
-    }
-
-    public void navigate(boolean requestFocus) {
-        getObject().navigate(requestFocus);
-    }
-
-    public boolean canNavigate() {
-        return true;
-    }
-
-    public boolean canNavigateToSource() {
-        return false;
-    }
-
-    public PsiManager getManager() {
-        return PsiManager.getInstance(getProject());
-    }
-
-    @NotNull
-    public PsiElement[] getChildren() {
-        List<PsiElement> children = new ArrayList<PsiElement>();
-        DBObjectListContainer childObjects = getObject().getChildObjects();
-        if (childObjects != null) {
-            Collection<DBObjectList<DBObject>> objectLists = childObjects.getObjectLists();
-            if (objectLists != null) {
-                for (DBObjectList objectList : objectLists) {
-                    children.add(NavigationPsiCache.getPsiDirectory(objectList));
-                }
-                return children.toArray(new PsiElement[children.size()]);
-            }
-        }
-
-        return new PsiElement[0];
-    }
-
-    public PsiElement getFirstChild() {
-        return null;
-    }
-
-    public PsiElement getLastChild() {
-        return null;
-    }
-
-    public PsiElement getNextSibling() {
-        return null;
-    }
-
-    public PsiElement getPrevSibling() {
-        return null;
-    }
-
-    public PsiFile getContainingFile() throws PsiInvalidElementAccessException {
-        return null;
-    }
-
-    public TextRange getTextRange() {
-        return null;
-    }
-
-    public int getStartOffsetInParent() {
-        return 0;
-    }
-
-    public int getTextLength() {
-        return 0;
-    }
-
-    public PsiElement findElementAt(int offset) {
-        return null;
-    }
-
-    public PsiReference findReferenceAt(int offset) {
-        return null;
-    }
-
-    public int getTextOffset() {
-        return 0;
-    }
-
-    public String getText() {
-        return null;
-    }
-
-    @NotNull
-    public char[] textToCharArray() {
-        return new char[0];
-    }
-
-    public PsiElement getNavigationElement() {
-        return this;
-    }
-
-    public PsiElement getOriginalElement() {
-        return this;
-    }
-
-    public boolean textMatches(@NotNull CharSequence text) {
-        return false;
-    }
-
-    public boolean textMatches(@NotNull PsiElement element) {
-        return false;
-    }
-
-    public boolean textContains(char c) {
-        return false;
-    }
-
-    public void accept(@NotNull PsiElementVisitor visitor) {
-
-    }
-
-    public void acceptChildren(@NotNull PsiElementVisitor visitor) {
-
-    }
-
-    public PsiElement copy() {
-        return null;
-    }
-
-    public PsiElement add(@NotNull PsiElement element) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public PsiElement addBefore(@NotNull PsiElement element, PsiElement anchor) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public PsiElement addAfter(@NotNull PsiElement element, PsiElement anchor) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public void checkAdd(@NotNull PsiElement element) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public PsiElement addRange(PsiElement first, PsiElement last) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public PsiElement addRangeBefore(@NotNull PsiElement first, @NotNull PsiElement last, PsiElement anchor) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public PsiElement addRangeAfter(PsiElement first, PsiElement last, PsiElement anchor) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public void delete() throws IncorrectOperationException {
-
-    }
-
-    public void checkDelete() throws IncorrectOperationException {
-
-    }
-
-    public void deleteChildRange(PsiElement first, PsiElement last) throws IncorrectOperationException {
-
-    }
-
-    public PsiElement replace(@NotNull PsiElement newElement) throws IncorrectOperationException {
-        return null;
-    }
-
-    public boolean isValid() {
-        return true;
-    }
-
-    public boolean isWritable() {
-        return false;
-    }
-
-    public PsiReference getReference() {
-        return null;
-    }
-
-    @NotNull
-    public PsiReference[] getReferences() {
-        return new PsiReference[0];
-    }
-
-    public <T> T getCopyableUserData(Key<T> key) {
-        return null;
-    }
-
-    public <T> void putCopyableUserData(Key<T> key, T value) {
-
-    }
-
-    public boolean processDeclarations(@NotNull PsiScopeProcessor processor, @NotNull ResolveState state, @Nullable PsiElement lastParent, @NotNull PsiElement place) {
-        return false;
-    }
-
-    public PsiElement getContext() {
-        return null;
-    }
-
-    public boolean isPhysical() {
-        return true;
-    }
-
-    @NotNull
-    public GlobalSearchScope getResolveScope() {
-        return EmptySearchScope.INSTANCE;
-    }
-
-    @NotNull
-    public SearchScope getUseScope() {
-        return EmptySearchScope.INSTANCE;
-    }
-
-    public boolean isEquivalentTo(PsiElement another) {
-        return false;
-    }
-
-    public Icon getIcon(int flags) {
-        return object.getIcon();
-    }
-
-    public <T> T getUserData(@NotNull Key<T> key) {
-        return null;
-    }
-
-    public <T> void putUserData(@NotNull Key<T> key, @Nullable T value) {
-
-    }
-
-    /*********************************************************
-     *                        PsiDirectory                   *
-     *********************************************************/
-    @NotNull
-    public VirtualFile getVirtualFile() {
-        return getObject().getVirtualFile();
-    }
-
-    public boolean processChildren(PsiElementProcessor<PsiFileSystemItem> processor) {
-        return false;
-    }
-
-    @NotNull
-    public PsiElement setName(@NotNull String name) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    public boolean isDirectory() {
-        return true;
-    }
-
-    public void checkSetName(String name) throws IncorrectOperationException {
-
-    }
-
-    @Override
-    public PsiDirectory getParentDirectory() {
-        return getParent();
-    }
-
-    @NotNull
-    @Override
-    public PsiDirectory[] getSubdirectories() {
-        return new PsiDirectory[0];
-    }
-
-    @NotNull
-    @Override
-    public PsiFile[] getFiles() {
-        return new PsiFile[0];
-    }
-
-    @Override
-    public PsiDirectory findSubdirectory(@NotNull String s) {
-        return null;
-    }
-
-    @Override
-    public PsiFile findFile(@NotNull String s) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public PsiDirectory createSubdirectory(@NotNull String s) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    @Override
-    public void checkCreateSubdirectory(@NotNull String s) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    @NotNull
-    @Override
-    public PsiFile createFile(@NotNull String s) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    @NotNull
-    @Override
-    public PsiFile copyFileFrom(@NotNull String s, @NotNull PsiFile psiFile) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
-
-    @Override
-    public void checkCreateFile(@NotNull String s) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Operation not supported");
-    }
+	@NotNull
+	@Override
+	public PsiFile[] getFiles()
+	{
+		return PsiFile.EMPTY_ARRAY;
+	}
+
+	@Override
+	public PsiDirectory findSubdirectory(@NotNull String s)
+	{
+		return null;
+	}
+
+	@Override
+	public PsiFile findFile(@NotNull String s)
+	{
+		return null;
+	}
+
+	@NotNull
+	@Override
+	public PsiDirectory createSubdirectory(@NotNull String s) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
+
+	@Override
+	public void checkCreateSubdirectory(@NotNull String s) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
+
+	@NotNull
+	@Override
+	public PsiFile createFile(@NotNull String s) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
+
+	@NotNull
+	@Override
+	public PsiFile copyFileFrom(@NotNull String s, @NotNull PsiFile psiFile) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
+
+	@Override
+	public void checkCreateFile(@NotNull String s) throws IncorrectOperationException
+	{
+		throw new IncorrectOperationException("Operation not supported");
+	}
 }
