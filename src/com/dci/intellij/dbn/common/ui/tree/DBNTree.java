08c1bdf10e34e7495c1e69f194978657129721b9
VISTALL <vistall.valeriy@gmail.com>
2015-07-04 13:30:25 +0300

    cleanup

--- src/com/dci/intellij/dbn/common/ui/tree/DBNTree.java
+++ src/com/dci/intellij/dbn/common/ui/tree/DBNTree.java
@@ -1,24 +1,26 @@
 package com.dci.intellij.dbn.common.ui.tree;
 
-import com.intellij.util.ui.Tree;
-import com.intellij.util.ui.UIUtil;
-
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
-public class DBNTree extends Tree {
-    public DBNTree() {
-        setTransferHandler(new DBNTreeTransferHandler());
-    }
+import com.intellij.ui.treeStructure.Tree;
 
-    public DBNTree(TreeModel treemodel) {
-        super(treemodel);
-        setTransferHandler(new DBNTreeTransferHandler());
-        setFont(UIUtil.getLabelFont());
-    }
+public class DBNTree extends Tree
+{
+	public DBNTree()
+	{
+		setTransferHandler(new DBNTreeTransferHandler());
+	}
 
-    public DBNTree(TreeNode root) {
-        super(root);
-        setTransferHandler(new DBNTreeTransferHandler());
-    }
+	public DBNTree(TreeModel treemodel)
+	{
+		super(treemodel);
+		setTransferHandler(new DBNTreeTransferHandler());
+	}
+
+	public DBNTree(TreeNode root)
+	{
+		super(root);
+		setTransferHandler(new DBNTreeTransferHandler());
+	}
 }
