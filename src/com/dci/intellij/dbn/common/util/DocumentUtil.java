7acbd9cc2ebe3664d3f60138d2d4543a204b8954
VISTALL <vistall.valeriy@gmail.com>
2015-08-28 08:33:57 +0300

    reformat

--- src/com/dci/intellij/dbn/common/util/DocumentUtil.java
+++ src/com/dci/intellij/dbn/common/util/DocumentUtil.java
@@ -16,12 +16,13 @@
 
 package com.dci.intellij.dbn.common.util;
 
+import org.mustbe.consulo.RequiredReadAction;
 import com.dci.intellij.dbn.common.editor.document.OverrideReadonlyFragmentModificationHandler;
 import com.dci.intellij.dbn.common.thread.CommandWriteActionRunner;
 import com.dci.intellij.dbn.connection.ConnectionHandler;
-import com.dci.intellij.dbn.language.common.SqlLikeLanguage;
 import com.dci.intellij.dbn.language.common.DBLanguageFile;
 import com.dci.intellij.dbn.language.common.DBLanguageSyntaxHighlighter;
+import com.dci.intellij.dbn.language.common.SqlLikeLanguage;
 import com.dci.intellij.dbn.language.common.psi.PsiUtil;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.ide.highlighter.HighlighterFactory;
@@ -39,88 +40,105 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 
-public class DocumentUtil {
+public class DocumentUtil
+{
+	public static void touchDocument(final Editor editor)
+	{
+		final Document document = editor.getDocument();
 
-
-    public static void touchDocument(final Editor editor) {
-        final Document document = editor.getDocument();
-
-        // restart highlighting
-        PsiFile file = DocumentUtil.getFile(editor);
-        if (file instanceof DBLanguageFile) {
-            DBLanguageFile dbLanguageFile = (DBLanguageFile) file;
-            SqlLikeLanguage sqlLikeLanguage = (SqlLikeLanguage) dbLanguageFile.getLanguage();
+		// restart highlighting
+		PsiFile file = DocumentUtil.getFile(editor);
+		if(file instanceof DBLanguageFile)
+		{
+			DBLanguageFile dbLanguageFile = (DBLanguageFile) file;
+			SqlLikeLanguage sqlLikeLanguage = (SqlLikeLanguage) dbLanguageFile.getLanguage();
 			ConnectionHandler connectionHandler = dbLanguageFile.getActiveConnection();
-			DBLanguageSyntaxHighlighter syntaxHighlighter = connectionHandler == null ?
-					sqlLikeLanguage.getFirstVersion().getSyntaxHighlighter() :
-					connectionHandler.getLanguageDialect(sqlLikeLanguage).getSyntaxHighlighter();
+			DBLanguageSyntaxHighlighter syntaxHighlighter = connectionHandler == null ? sqlLikeLanguage
+					.getFirstVersion().getSyntaxHighlighter() : connectionHandler.getLanguageDialect(sqlLikeLanguage)
+					.getSyntaxHighlighter();
 
-			EditorHighlighter editorHighlighter = HighlighterFactory.createHighlighter(syntaxHighlighter, editor.getColorsScheme());
+			EditorHighlighter editorHighlighter = HighlighterFactory.createHighlighter(syntaxHighlighter,
+					editor.getColorsScheme());
 			((EditorEx) editor).setHighlighter(editorHighlighter);
 		}
 
-        new CommandWriteActionRunner(editor.getProject()) {
-            public void run() {
-                // touch the editor to trigger parsing
+		new CommandWriteActionRunner(editor.getProject())
+		{
+			@Override
+			public void run()
+			{
+				// touch the editor to trigger parsing
 
-                int length = document.getTextLength();
-                document.insertString(length, " ");
-                document.deleteString(length, length + 1);
-                refreshEditorAnnotations(editor.getProject());
-            }
-        }.start();
+				int length = document.getTextLength();
+				document.insertString(length, " ");
+				document.deleteString(length, length + 1);
+				refreshEditorAnnotations(editor.getProject());
+			}
+		}.start();
 
-        //refreshEditorAnnotations(editor.getProject());
-    }
+		//refreshEditorAnnotations(editor.getProject());
+	}
 
 
-    public static void refreshEditorAnnotations(Project project) {
-        DaemonCodeAnalyzer.getInstance(project).restart();
-    }
+	public static void refreshEditorAnnotations(Project project)
+	{
+		DaemonCodeAnalyzer.getInstance(project).restart();
+	}
 
-    public static Document getDocument(PsiFile file) {
-        return PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
-    }
+	public static Document getDocument(PsiFile file)
+	{
+		return PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
+	}
 
-    public static PsiFile getFile(Editor editor) {
-        Project project = editor.getProject();
-        return project == null ? null : PsiUtil.getPsiFile(project, editor.getDocument());
-    }
+	public static PsiFile getFile(Editor editor)
+	{
+		Project project = editor.getProject();
+		return project == null ? null : PsiUtil.getPsiFile(project, editor.getDocument());
+	}
 
-    public static void createGuardedBlock(Document document, String reason, boolean highlight) {
-        createGuardedBlock(document, 0, document.getTextLength(), reason);
-        if (!highlight) {
-            Editor[] editors = EditorFactory.getInstance().getEditors(document);
-            for (Editor editor : editors) {
-                ColorKey key = ColorKey.find("READONLY_FRAGMENT_BACKGROUND");
-                EditorColorsScheme scheme = editor.getColorsScheme();
-                scheme.setColor(key, scheme.getDefaultBackground());
-            }
-        }
-    }
+	public static void createGuardedBlock(Document document, String reason, boolean highlight)
+	{
+		createGuardedBlock(document, 0, document.getTextLength(), reason);
+		if(!highlight)
+		{
+			Editor[] editors = EditorFactory.getInstance().getEditors(document);
+			for(Editor editor : editors)
+			{
+				ColorKey key = ColorKey.find("READONLY_FRAGMENT_BACKGROUND");
+				EditorColorsScheme scheme = editor.getColorsScheme();
+				scheme.setColor(key, scheme.getDefaultBackground());
+			}
+		}
+	}
 
-    public static void createGuardedBlock(Document document, int startOffset, int endOffset, String reason) {
-        RangeMarker rangeMarker = document.createGuardedBlock(startOffset, endOffset);
-        rangeMarker.setGreedyToLeft(true);
-        rangeMarker.setGreedyToRight(true);
-        document.putUserData(OverrideReadonlyFragmentModificationHandler.GUARDED_BLOCK_REASON, reason);
-    }
+	public static void createGuardedBlock(Document document, int startOffset, int endOffset, String reason)
+	{
+		RangeMarker rangeMarker = document.createGuardedBlock(startOffset, endOffset);
+		rangeMarker.setGreedyToLeft(true);
+		rangeMarker.setGreedyToRight(true);
+		document.putUserData(OverrideReadonlyFragmentModificationHandler.GUARDED_BLOCK_REASON, reason);
+	}
 
-    public static void removeGuardedBlock(Document document) {
-        removeGuardedBlock(document, 0, document.getTextLength());
-    }
+	public static void removeGuardedBlock(Document document)
+	{
+		removeGuardedBlock(document, 0, document.getTextLength());
+	}
 
-    public static void removeGuardedBlock(Document document, int startOffset, int endOffset) {
-        RangeMarker rangeMarker = document.getRangeGuard(startOffset, endOffset);
-        document.removeGuardedBlock(rangeMarker);
-        document.putUserData(OverrideReadonlyFragmentModificationHandler.GUARDED_BLOCK_REASON, null);
-    }
+	public static void removeGuardedBlock(Document document, int startOffset, int endOffset)
+	{
+		RangeMarker rangeMarker = document.getRangeGuard(startOffset, endOffset);
+		document.removeGuardedBlock(rangeMarker);
+		document.putUserData(OverrideReadonlyFragmentModificationHandler.GUARDED_BLOCK_REASON, null);
+	}
 
-    public static VirtualFile getVirtualFile(Editor editor) {
-        return FileDocumentManager.getInstance().getFile(editor.getDocument());
-    }
+	public static VirtualFile getVirtualFile(Editor editor)
+	{
+		return FileDocumentManager.getInstance().getFile(editor.getDocument());
+	}
 
-    public static Document getDocument(VirtualFile virtualFile) {
-        return FileDocumentManager.getInstance().getDocument(virtualFile);
-    }
+	@RequiredReadAction
+	public static Document getDocument(VirtualFile virtualFile)
+	{
+		return FileDocumentManager.getInstance().getDocument(virtualFile);
+	}
 }
