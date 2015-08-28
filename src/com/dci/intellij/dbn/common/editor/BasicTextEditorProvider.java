d7533b16bf8da766a93e72d42e0eb0d8f0d09956
VISTALL <vistall.valeriy@gmail.com>
2015-08-28 08:37:32 +0300

    dont throw exception about read action

--- src/com/dci/intellij/dbn/common/editor/BasicTextEditorProvider.java
+++ src/com/dci/intellij/dbn/common/editor/BasicTextEditorProvider.java
@@ -16,53 +16,82 @@
 
 package com.dci.intellij.dbn.common.editor;
 
+import javax.swing.Icon;
+
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
 import com.dci.intellij.dbn.common.thread.SimpleLaterInvocator;
 import com.dci.intellij.dbn.common.util.DocumentUtil;
 import com.dci.intellij.dbn.common.util.EditorUtil;
 import com.dci.intellij.dbn.vfs.DatabaseEditableObjectFile;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileEditorProvider;
 import com.intellij.openapi.fileEditor.FileEditorState;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jdom.Element;
-import org.jetbrains.annotations.NotNull;
 
-import javax.swing.Icon;
+public abstract class BasicTextEditorProvider implements FileEditorProvider, ApplicationComponent, DumbAware
+{
+	@Override
+	@NotNull
+	public FileEditorState readState(@NotNull Element sourceElement,
+			@NotNull final Project project,
+			@NotNull final VirtualFile virtualFile)
+	{
+		BasicTextEditorState editorState = new BasicTextEditorState();
+		Document document = ApplicationManager.getApplication().runReadAction(new Computable<Document>()
+		{
+			@Override
+			public Document compute()
+			{
+				return DocumentUtil.getDocument(virtualFile);
+			}
+		});
+		editorState.readState(sourceElement, project, document);
+		return editorState;
+	}
 
-public abstract class BasicTextEditorProvider implements FileEditorProvider, ApplicationComponent, DumbAware {
-    @NotNull
-    public FileEditorState readState(@NotNull Element sourceElement, @NotNull final Project project, @NotNull final VirtualFile virtualFile) {
-        BasicTextEditorState editorState = new BasicTextEditorState();
-        Document document = DocumentUtil.getDocument(virtualFile);
-        editorState.readState(sourceElement, project, document);
-        return editorState;
-    }
+	@Override
+	public void writeState(@NotNull FileEditorState state, @NotNull Project project, @NotNull Element targetElement)
+	{
+		if(state instanceof BasicTextEditorState)
+		{
+			BasicTextEditorState editorState = (BasicTextEditorState) state;
+			editorState.writeState(targetElement, project);
+		}
+	}
 
-    public void writeState(@NotNull FileEditorState state, @NotNull Project project, @NotNull Element targetElement) {
-        if (state instanceof BasicTextEditorState) {
-            BasicTextEditorState editorState = (BasicTextEditorState) state;
-            editorState.writeState(targetElement, project);
-        }
-    }
+	protected void updateTabIcon(final DatabaseEditableObjectFile databaseFile,
+			final BasicTextEditor textEditor,
+			final Icon icon)
+	{
+		new SimpleLaterInvocator()
+		{
+			@Override
+			public void run()
+			{
+				EditorUtil.setEditorIcon(databaseFile, textEditor, icon);
+			}
+		}.start();
+	}
 
-    protected void updateTabIcon(final DatabaseEditableObjectFile databaseFile, final BasicTextEditor textEditor, final Icon icon) {
-        new SimpleLaterInvocator() {
-            public void run() {
-                EditorUtil.setEditorIcon(databaseFile, textEditor, icon);
-            }
-        }.start();
-    }
+	/**
+	 * ******************************************************
+	 * ApplicationComponent                   *
+	 * *******************************************************
+	 */
+	@Override
+	public void initComponent()
+	{
+	}
 
-    /*********************************************************
-     *                ApplicationComponent                   *
-     *********************************************************/
-    public void initComponent() {
-    }
+	@Override
+	public void disposeComponent()
+	{
 
-    public void disposeComponent() {
-
-    }
+	}
 }
--- src/com/dci/intellij/dbn/editor/console/SQLConsoleEditorProvider.java
+++ src/com/dci/intellij/dbn/editor/console/SQLConsoleEditorProvider.java
@@ -16,75 +16,105 @@
 
 package com.dci.intellij.dbn.editor.console;
 
+import java.awt.BorderLayout;
+
+import org.jdom.Element;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import com.dci.intellij.dbn.common.editor.BasicTextEditorProvider;
 import com.dci.intellij.dbn.common.util.DocumentUtil;
 import com.dci.intellij.dbn.editor.console.ui.SQLConsoleEditorToolbarForm;
 import com.dci.intellij.dbn.vfs.SQLConsoleFile;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
 import com.intellij.openapi.fileEditor.FileEditorState;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jdom.Element;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.BorderLayout;
 
 
-public class SQLConsoleEditorProvider extends BasicTextEditorProvider {
+public class SQLConsoleEditorProvider extends BasicTextEditorProvider
+{
+	@Override
+	public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile)
+	{
+		return virtualFile instanceof SQLConsoleFile;
+	}
 
-    public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile) {
-        return virtualFile instanceof SQLConsoleFile;
-    }
+	@Override
+	@NotNull
+	public FileEditorState readState(@NotNull Element sourceElement,
+			@NotNull Project project,
+			@NotNull final VirtualFile virtualFile)
+	{
+		SQLConsoleEditorState editorState = new SQLConsoleEditorState();
+		Document document = ApplicationManager.getApplication().runReadAction(new Computable<Document>()
+		{
+			@Override
+			public Document compute()
+			{
+				return DocumentUtil.getDocument(virtualFile);
+			}
+		});
+		editorState.readState(sourceElement, project, document);
+		return editorState;
+	}
 
-    @NotNull
-    public FileEditorState readState(@NotNull Element sourceElement, @NotNull Project project, @NotNull VirtualFile virtualFile) {
-        SQLConsoleEditorState editorState = new SQLConsoleEditorState();
-        Document document = DocumentUtil.getDocument(virtualFile);
-        editorState.readState(sourceElement, project, document);
-        return editorState;
-    }
+	@Override
+	public void writeState(@NotNull FileEditorState state, @NotNull Project project, @NotNull Element targetElement)
+	{
+		if(state instanceof SQLConsoleEditorState)
+		{
+			SQLConsoleEditorState editorState = (SQLConsoleEditorState) state;
+			editorState.writeState(targetElement, project);
+		}
+	}
 
-    public void writeState(@NotNull FileEditorState state, @NotNull Project project, @NotNull Element targetElement) {
-        if (state instanceof SQLConsoleEditorState) {
-            SQLConsoleEditorState editorState = (SQLConsoleEditorState) state;
-            editorState.writeState(targetElement, project);
-        }
-    }
+	@Override
+	@NotNull
+	public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file)
+	{
+		SQLConsoleEditor editor = new SQLConsoleEditor(project, (SQLConsoleFile) file, "SQL Console");
+		SQLConsoleEditorToolbarForm toolbarForm = new SQLConsoleEditorToolbarForm(editor);
+		editor.getComponent().add(toolbarForm.getComponent(), BorderLayout.NORTH);
+		return editor;
+	}
 
-    @NotNull
-    public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file) {
-        SQLConsoleEditor editor = new SQLConsoleEditor(project, (SQLConsoleFile) file, "SQL Console");
-        SQLConsoleEditorToolbarForm toolbarForm = new SQLConsoleEditorToolbarForm(editor);
-        editor.getComponent().add(toolbarForm.getComponent(), BorderLayout.NORTH);
-        return editor;
-    }
+	@Override
+	public void disposeEditor(@NotNull FileEditor editor)
+	{
+		editor.dispose();
+	}
 
-    public void disposeEditor(@NotNull FileEditor editor) {
-        editor.dispose();
-    }
+	@Override
+	@NotNull
+	public FileEditorPolicy getPolicy()
+	{
+		return FileEditorPolicy.HIDE_DEFAULT_EDITOR;
+	}
 
-    @NotNull
-    public FileEditorPolicy getPolicy() {
-        return FileEditorPolicy.HIDE_DEFAULT_EDITOR;
-    }
+	@Override
+	@NotNull
+	@NonNls
+	public String getEditorTypeId()
+	{
+		return "SQLConsole";
+	}
 
-    @NotNull
-    @NonNls
-    public String getEditorTypeId() {
-        return "SQLConsole";
-    }
+	/**
+	 * ******************************************************
+	 * ApplicationComponent                   *
+	 * *******************************************************
+	 */
 
-    /*********************************************************
-     *                ApplicationComponent                   *
-     *********************************************************/
-
-    @NonNls
-    @NotNull
-    public String getComponentName() {
-        return "DBNavigator.SQLConsoleEditorProvider";
-    }
+	@Override
+	@NonNls
+	@NotNull
+	public String getComponentName()
+	{
+		return "DBNavigator.SQLConsoleEditorProvider";
+	}
 
 }
